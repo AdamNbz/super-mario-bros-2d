9c544d39d112b8bbd58266643e2c4bebf8d00bb4
Nguyễn Võ Ngọc Bảo <contact.adamnguyen@gmail.com>
2025-09-22 00:15:23 +0700

    Enemies, Player Death & Level Reset

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,15 @@
+using System;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance { get; private set; }
+
+    public int world { get; private set; }
+    public int stage { get; private set; }
+    public int lives { get; private set; }
 
     private void Awake()
     {
@@ -17,5 +24,47 @@ public class GameManager : MonoBehaviour
     private void OnDestroy()
     {
         if (instance == this) instance = null;
+    }
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    private void NewGame()
+    {
+        lives = 3;
+
+        LoadLevel(1, 1);
+    }
+
+    private void LoadLevel(int world, int stage)
+    {
+        this.world = world;
+        this.stage = stage;
+
+        SceneManager.LoadScene($"{world}-{stage}");
+    }
+
+    public void NextLevel()
+    {
+        LoadLevel(world, stage + 1);
+    }
+
+    public void ResetLevel(float delay)
+    {
+        Invoke(nameof(ResetLevel), delay);
+    }
+
+    public void ResetLevel()
+    {
+        lives--;
+        if (lives > 0) LoadLevel(world, stage);
+        else GameOver();
+    }
+
+    private void GameOver()
+    {
+        Invoke(nameof(NewGame), 3f);
     }
 }
